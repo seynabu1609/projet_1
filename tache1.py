11c54689d4c6d01b664dfc6a92ee2048cd56f9c0
seynabu1609 <nabusey003@gmail.com>
2024-06-11 13:00:32 +0000

    tache1 termine

---
+++ tache1.py
@@ -0,0 +1,6 @@
+x = 1
+
+if x == 2:
+    print("Le test est vrai!")
+
+ 
---
+++ tache2.py
@@ -0,0 +1,10 @@
+x = 1
+
+# Si x == 2 afficher le message 'Le test est vrai'
+if x == 2:
+    print("Le test est vrai!")
+# Si x == 2 afficher le message 'Le test est faux'
+if x == 2:
+    print("Le test est vrai!")
+else:
+    print("Le test est faux !")    
